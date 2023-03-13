de9ee76e93769a7b2ff130ff95b18f5a89638148
Calin Andreca <calin.andreca@gmail.com>
2023-03-13 21:05:59 +0200

    Interface

---
+++ src/interfaces/McDonalds.java
@@ -0,0 +1,30 @@
+package interfaces;
+
+public class McDonalds implements FoodProvider {
+    private static double pricePerItem = 15;
+    private int quantity = 0;
+    private double amountToPay = 0;
+
+    @Override
+    public boolean pay(double amount) {
+        if (amount > 0 && amount <= amountToPay) {
+            amountToPay -= amount;
+            System.out.println("MCDonalds has been pay " + amount);
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    public String deliverFood() {
+        quantity++;
+        amountToPay += pricePerItem;
+        return "burger";
+    }
+
+    @Override
+    public int getDeliveredQuantity() {
+        return quantity;
+    }
+}
+
