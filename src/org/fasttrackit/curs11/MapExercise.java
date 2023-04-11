fc44bb3be51f3ac4e1f26b85990b68ab94eb6ad1
Calin Andreca <calin.andreca@gmail.com>
2023-04-11 20:03:17 +0300

    Homework10

--- src/org/fasttrackit/curs11/MapExercise.java
+++ src/org/fasttrackit/curs11/MapExercise.java
@@ -1,8 +1,6 @@
 package org.fasttrackit.curs11;
 
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Set;
+import java.util.*;
 
 public class MapExercise {
     public static void main(String[] args) {
@@ -11,17 +9,29 @@ public class MapExercise {
         nameToAge.put("Alex", 31);
         nameToAge.put("John", 20);
         nameToAge.put("Alex", 40);
+        for (int i=0; i<1000000; i++){
+            nameToAge.put("Name " + i,i);
+        }
+        long time, time2;
 
         System.out.println(nameToAge);
-
+        long start = System.currentTimeMillis();
         Set<String> keySet = nameToAge.keySet();
         for (String key:keySet){
             System.out.println(nameToAge.get(key));
         }
 
+        time = System.currentTimeMillis() - start;
+
         Set<Map.Entry<String, Integer>> entries = nameToAge.entrySet();
         for (Map.Entry<String, Integer> entry: entries){
             System.out.println(entry.getValue());
         }
+        time2 = System.currentTimeMillis() - start;
+        System.out.println("execution time " + time);
+        System.out.println("execution time2 " + time2);
+
+
     }
+
 }
